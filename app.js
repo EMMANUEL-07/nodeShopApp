afe68c8006655fb242ed39c45b2ed58f15c5810c
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2021-07-09 19:06:49 +0100

    beginning of adding users in mongodb

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const MongoConnect = require('./util/database')
+const MongoConnect = require('./util/database').MongoConnect
+const User = require('./models/user')
 
 const app = express();
 
@@ -12,27 +13,27 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
  
 const adminRoutes = require('./routes/admin');
-/*const shopRoutes = require('./routes/shop');
- */
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-/* app.use((req, res, next) => {
-   User.findByPk(1)
-   .then( user => {
+app.use((req, res, next) => {
+   User.findById('60e88fc8bdfaa21dce586b9d')
+   .then(user => {
       req.user = user
       next();
    })
    .catch( err => console.log(err))
+
+   next();
 })
- */
  
 app.use('/admin', adminRoutes);
 
-/*app.use(shopRoutes);*/
+app.use(shopRoutes);
 app.use(errorController.get404);
 
 MongoConnect(() => {
-
    app.listen(3000)
 })
