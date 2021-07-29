70015d60376af7b44f744036c5bf824c31c9a435
EMMANUEL OYEKAN <emmanueloyekan33@gmail.com>
2021-07-29 13:30:41 +0100

    Reset password and Authorization

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,6 +35,24 @@ exports.postAddProduct = (req, res, next) => {
     });
 };
 
+
+exports.getProducts = (req, res, next) => {
+  Product.find({userId: req.user._id})
+    // .select('title price -_id')
+    // .populate('userId', 'name')
+    .then(products => {
+      console.log(products);
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products',
+        
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
@@ -63,41 +81,34 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  
 
   Product.findById(prodId)
     .then(product => {
+
+      if(product.userId.toString() !== req.user._id.toString()){
+        return res.redirect('/')
+      }
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
       product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
-};
-
-exports.getProducts = (req, res, next) => {
-  Product.find()
-    // .select('title price -_id')
-    // .populate('userId', 'name')
-    .then(products => {
-      console.log(products);
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products',
-        
+      
+      return product
+      .save()
+      .then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
       });
     })
     .catch(err => console.log(err));
 };
 
+
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByIdAndRemove(prodId)
+  
+  Product.deleteOne({_id: prodId, userId: req.user._id})
     .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
